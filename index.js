df4bd0e6b1d501e753dbdf795494d261c962cd8e
Rachell Aristo <123459774+sleepycodingcat@users.noreply.github.com>
2024-07-09 09:06:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const decreaseBtn = document.getElementById("decreaseBtn")
+const increaseBtn = document.getElementById("increaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const countLabel = document.getElementById("countLabel")
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count += 1;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    if (count != 0){
+        count -= 1;
+    }
+    countLabel.textContent = count;
+}
+
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+
 
